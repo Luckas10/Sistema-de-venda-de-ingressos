38c14b2e2093163744a24cad484f07e78510b83e
Luckas10 <johndescraft@gmail.com>
2023-11-22 21:20:34 -0300

    initial commit

---
+++ Codes/interface.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+
+class App:
+    def __init__(self):
+        self.janela = Tk()
+        self.janela.geometry("1000x650")
+
+        self.janela.mainloop()
+
+
+app = App()
